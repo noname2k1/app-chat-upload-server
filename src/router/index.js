cee5efb7f970022983c4e4929fb12873f60842b1
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-08-12 16:56:07 +0700

    init chat server

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,10 @@ const prefix = '/api';
 const authRoutes = (app) => {
     app.use(`${prefix}/auth`, authRoute);
     app.use(`${prefix}/profile`, isAuthenticated, profileRoute);
+    app.get('/', (req, res) => res.status(200).json('Welcome to Auth API'));
 };
 const chatRoutes = (app) => {
     app.use(`${prefix}/chat`, isAuthenticated, chatRoute);
-};
-const welComeRoutes = (app) => {
     app.get('/', (req, res) => res.status(200).json('Welcome to Chat API'));
 };
 module.exports = { authRoutes, chatRoutes, welComeRoutes };
