afe9d21aa462bdf6fe6dfe76ab23317c65dddeae
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-08-12 14:26:01 +0700

    long time no edit

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ const profileRoute = require('./profileRoute');
 const chatRoute = require('./chatRoute');
 const { isAuthenticated } = require('../middlewares/authMiddleWares');
 const authRoutes = (app) => {
+    app.use('/', (req, res) => res.write('Welcome to Chat API'));
     app.use('/api/auth', authRoute);
     app.use('/api/profile', isAuthenticated, profileRoute);
 };
