c50fdf462c4aa7e95ae89917199cb0ffe0127cfe
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-08-12 16:27:11 +0700

    test

--- api/index.js
+++ api/index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-const { welComeRoutes } = require('../src/router');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-welComeRoutes(app);
-
-const POST = 9999;
-
-app.listen(POST, () => console.log(`welcome is running on port ${POST}`));
+const app = require('../src/server');
 
 module.exports = app;
