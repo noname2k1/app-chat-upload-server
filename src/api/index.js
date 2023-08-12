d26a7631d3dfb414bcf92db76c1cd425f0961957
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-08-12 16:04:28 +0700

    restructuring

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-const { welComeRoutes } = require('../router');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-welComeRoutes(app);
+const welcome = require('../server');
 
-const POST = 9999;
-
-app.listen(POST, () => console.log(`welcome is running on port ${POST}`));
-
-module.exports = app;
+module.exports = welcome;
---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const { welComeRoutes } = require('../router');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+welComeRoutes(app);
+
+const POST = 9999;
+
+app.listen(POST, () => console.log(`welcome is running on port ${POST}`));
+
+module.exports = app;
